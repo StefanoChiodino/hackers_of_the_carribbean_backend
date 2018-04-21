38b4667a1181f86ada5afc84ef1ba0fab325d969
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2018-04-21 16:15:32 +0100

    Seed data

--- fight/models.py
+++ fight/models.py
@@ -4,30 +4,27 @@ from django.db import models
 
 
 class Comeback(models.Model):
-    comeback_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     comeback_text = models.CharField(max_length=1000)
 
 
 class Insult(models.Model):
-    insult_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     insult_text = models.CharField(max_length=1000)
-    correct_comeback_id = models.ForeignKey(Comeback, on_delete=models.CASCADE)
+    correct_comeback = models.ForeignKey(Comeback, on_delete=models.CASCADE)
 
 
 class Step(models.Model):
-    index = models.IntegerField(primary_key=True)
-    insult_id = models.ForeignKey(Insult, on_delete=models.CASCADE)
+    index = models.IntegerField(unique=True)
+    insult = models.ForeignKey(Insult, on_delete=models.CASCADE)
 
 
 class Fight(models.Model):
-    fight_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    current_fight = models.ForeignKey(Step, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.fight_id)
+    # id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    current_fight = models.ForeignKey(Step, on_delete=models.CASCADE, null=True)
 
 
 class Game(models.Model):
-    game_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    # id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     health = models.IntegerField()
-    current_fight = models.ForeignKey(Fight, on_delete=models.CASCADE)
+    current_fight = models.ForeignKey(Fight, on_delete=models.CASCADE, null=True)
